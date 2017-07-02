8444c0d1fbaf8fd1bc3d83a587a0cdbec76c916a
Michael Chow <machow@princeton.edu>
2017-07-02 18:09:52 +0200

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,1 +1,5 @@
-apt-get update && apt-get install -y libxml2-dev
+apt-get update
+apt-get install -y libxml2-dev
+apt-get install -y --no-install-recommends clang
+
+mkdir -p $HOME/.R && printf "CXX=clang++ -ftemplate-depth-256\nCC=clang\n" > $HOME/.R/Makevars
